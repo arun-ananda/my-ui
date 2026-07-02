f9238adb43a79279de2aad05485ff8bf120bd0f0
Arun Kumar <arun.kumar.va@gmail.com>
2026-07-02 13:31:19 +0530

    added data to posts component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Posts } from './posts/posts';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Posts],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('my-ui');
+  posts=[
+        {id:1},
+        {id:2},
+        {id:3}
+  ];
+    
 }
--- src/app/posts/posts.ts
+++ src/app/posts/posts.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './posts.html',
   styleUrl: './posts.scss',
 })
-export class Posts {}
+export class Posts {
+  @Input() userPosts: any[] = [];
+}
